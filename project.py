74f2a6627f9b97ce65f260899f042e8108b84b57
Sergey Martynov <sergo.martynov@gmail.com>
2017-01-28 17:14:17 +0300

    - correction of record to db (correct order of name/email/pass sequence) was added - cabinet page was added for the user from db - login mechanism was added

--- project.py
+++ project.py
@@ -10,18 +10,29 @@ def index():
 
 @my_flask_app.route('/registration/', methods=['POST'])
 def registration():
-    new_user = User(request.form.get('name'), request.form.get('email'),
-                    request.form.get('password'))
+    new_user = User(
+        name=request.form.get('name'),
+        email=request.form.get('email'),
+        password=request.form.get('password')
+    )
     db_session.add(new_user)
     db_session.commit()
     return redirect("/", code=302)
 
 @my_flask_app.route('/login/', methods=['POST'])
 def login():
-    return render_template('login.html', email=request.form.get('email'),
-                           password=request.form.get('password'))
+#return render_template('login.html', email=request.form.get('email'),
+#password=request.form.get('password'))
+    user_name = request.form.get('email')
+    user_password = request.form.get('password')
+    user = User.query.filter(User.name == user_name, User.password == user_password).first()
+    if user:
+        return redirect('/cabinet/')
+    return redirect("/", code=302)
 
+@my_flask_app.route('/cabinet/')
+def cabinet():
+    return render_template('cabinet.html', title="Cabinet")
 
 if __name__ == '__main__':
     my_flask_app.run(debug=True, port=5005)
-    
